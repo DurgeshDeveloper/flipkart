b492256849134067114d03441c17c92e6c8ff116
DurgeshDeveloper <gdurgesh79@gmail.com>
2022-12-03 08:03:19 +0530

    code

--- javascript.js
+++ javascript.js
@@ -6,3 +6,12 @@ const changeMode = () => {
 } 
 
 
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('main').style.opacity='0.5';
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity='1';
+}
